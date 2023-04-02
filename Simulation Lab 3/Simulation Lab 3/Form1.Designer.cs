2270735116f6e0702702277f794414299195b2af
Комардин Влад <99380380+Skotch42@users.noreply.github.com>
2023-04-02 18:03:31 +0700

    Add files via upload

---
+++ Simulation Lab 3/Simulation Lab 3/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Simulation_Lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btStart = new System.Windows.Forms.Button();
+            this.edRule = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.edRule)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rule";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(12, 294);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(177, 36);
+            this.btStart.TabIndex = 2;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // edRule
+            // 
+            this.edRule.Location = new System.Drawing.Point(69, 247);
+            this.edRule.Maximum = new decimal(new int[] {
+            256,
+            0,
+            0,
+            0});
+            this.edRule.Name = "edRule";
+            this.edRule.Size = new System.Drawing.Size(120, 22);
+            this.edRule.TabIndex = 3;
+            this.edRule.Value = new decimal(new int[] {
+            108,
+            0,
+            0,
+            0});
+            this.edRule.ValueChanged += new System.EventHandler(this.edRule_ValueChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.ColumnHeadersVisible = false;
+            this.dataGridView.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridView.Location = new System.Drawing.Point(195, 46);
+            this.dataGridView.MultiSelect = false;
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.RowHeadersWidth = 62;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(593, 516);
+            this.dataGridView.TabIndex = 1;
+            this.dataGridView.SelectionChanged += new System.EventHandler(this.dataGridView_SelectionChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 648);
+            this.Controls.Add(this.edRule);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.edRule)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.NumericUpDown edRule;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
+
---
+++ Simulation Lab 3/Simulation Lab 3/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulation_Lab_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void dataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            dataGridView.ClearSelection();
+        }
+
+        const int A = 20;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int rule = (int)edRule.Value;
+            int[] rule_in_2 = new int[8];
+            int[,] table = new int[A, A];
+
+            string s = Convert.ToString(rule, 2);
+            int n = Convert.ToInt32(s);
+
+            for (int j = 7; j >= 0; j--)
+            {
+                rule_in_2[j] = n % 10;
+                n = n / 10;
+            }
+
+            table[0, 0] = 0; table[0, 1] = 0; table[0, 2] = 0; table[0, 3] = 0; 
+            table[0, 4] = 0; table[0, 5] = 0; table[0, 6] = 0; table[0, 7] = 0; 
+            table[0, 8] = 0; table[0, 9] = 1; table[0, 10] = 0; table[0, 11] = 0; 
+            table[0, 12] = 0; table[0, 13] = 0; table[0, 14] = 0; table[0, 15] = 0; 
+            table[0, 16] = 0; table[0, 17] = 0; table[0, 18] = 0; table[0, 19] = 0;
+
+            for (int i = 0; i < A - 1; i++) 
+            {
+                int a = table[i, A - 1] * 100 + table[i, 0] * 10 + table[i, 1];
+                string s1 = Convert.ToString(a);
+                int b = Convert.ToInt32(s1, 2);
+
+                for (int k = 0; k < 8; k++) 
+                {
+                    if (k == b)
+                    {
+                        table[i + 1, 0] = rule_in_2[k];
+                    }
+                }
+
+                for (int j = 1; j < A - 1; j++)
+                {
+                    a = table[i, j - 1] * 100 + table[i, j] * 10 + table[i, j + 1];
+                    s1 = Convert.ToString(a);
+                    b = Convert.ToInt32(s1, 2);
+
+                    for (int k = 0; k < 8; k++) 
+                    {
+                        if (k == b)
+                        {
+                            table[i + 1, j] = rule_in_2[k];
+                        }
+                    }
+                }
+
+                a = table[i, A - 2] * 100 + table[i, A - 1] * 10 + table[i, 0];
+                s1 = Convert.ToString(a);
+                b = Convert.ToInt32(s1, 2);
+
+                for (int l = 0; l < 8; l++) 
+                {
+                    if (l == b)
+                    {
+                        table[i + 1, A - 1] = rule_in_2[l];
+                    }
+                }
+            }
+
+            dataGridView.ColumnCount = A; 
+            dataGridView.RowCount = A;
+            for (int i = 0; i < A; i++)
+            {
+                for (int j = 0; j < A; j++)
+                {
+                    if (table[i, j] == 1)
+                    {
+                        dataGridView.Rows[i].Cells[j].Style.BackColor = Color.Blue;
+                    }
+                }
+            }
+        }
+
+        private void edRule_ValueChanged(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
